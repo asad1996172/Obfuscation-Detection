3ea6703f59d91c0d29be82a7ff1abb2e414cb87c
asad1996172 <asad007mahmood@gmail.com>
2019-11-25 19:24:03 -0600

    Making feature extraction work

---
+++ predict.py
@@ -0,0 +1,70 @@
+import argparse
+import sys
+import logging
+import utils
+
+
+def evaluate_text(text, dataset_type, language_model, language_model_output_type, feature_type, classifier):
+    """
+    :param text: Input text for obfuscation detection
+    :param dataset_type: The dataset used to train the obfuscation detection model.
+    :param language_model: The Language Model used to train the obfuscation detection model.
+    :param language_model_output_type: The Language Model output type used to train the obfuscation detection model.
+    :param feature_type: The feature type used to train the obfuscation detection model.
+    :param classifier: The classifier used to train the obfuscation detection model.
+    :return: a tuple containing three things (prediction, probability_obfuscated, probability_evaded)
+    """
+
+    req_model_path = 'models/' + '_'.join(
+        [dataset_type, language_model, language_model_output_type, classifier, feature_type])
+    clf = utils.load_model(req_model_path)
+    features = utils.extract_features(text, language_model, language_model_output_type, feature_type)
+    print(features)
+    print(len(features))
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    # Input parameters
+    parser.add_argument("--text", "-t", help="Input string containing text for obfuscation detection", default='')
+    parser.add_argument("--file_path", "-fp", help="Path of file containing text for obfuscation detection", default='')
+
+    # Obfuscation detection parameters
+    parser.add_argument("--dataset_type", "-dt", help="Dataset on which the model was trained on", default='')
+    parser.add_argument("--language_model", "-lm", help="Language model used to trained the model", default='bert_base')
+    parser.add_argument("--language_model_output_type", "-lmot",
+                        help="Type of output used from language model for training the model", default='probs')
+    parser.add_argument("--feature_type", "-ft", help="Type of feature used for training model", default='vgg19')
+    parser.add_argument("--classifier", "-c", help="Classifier used for training model", default='ann')
+
+    # parsing arguments
+    args = parser.parse_args()
+
+    text = args.text
+    file_path = args.file_path
+
+    dataset_type = args.dataset_type.lower()
+    language_model = args.language_model.lower()
+    language_model_output_type = args.language_model_output_type.lower()
+    feature_type = args.feature_type.lower()
+    classifier = args.classifier.lower()
+
+    # error/warning handling
+    if (text == '') and (file_path == ''):
+        logging.error('Please give either text or file path as input')
+        sys.exit()
+    if (text != '') and (file_path != ''):
+        logging.warning('Both text and file path given as input. Using only text for obfuscation detection')
+        file_path = ''
+    if dataset_type == '':
+        logging.error('Please give a dataset name on which the required model was trained on')
+        sys.exit()
+
+    # extracting text required for obfuscation detection
+    if text == '':
+        text = utils.read_file(file_path)
+
+    evaluate_text(text, dataset_type, language_model, language_model_output_type, feature_type, classifier)
+
+if __name__ == '__main__':
+    main()
--- utils.py
+++ utils.py
@@ -1,4 +1,28 @@
 import os
+import io
+import pickle
+import GLTR
+from matplotlib import pyplot as plt
+from keras.applications.vgg19 import VGG19
+from keras.preprocessing import image
+from keras.applications.vgg19 import preprocess_input
+from keras.models import Model
+import numpy as np
+import logging
+
+def get_ranks_and_probs(payloads_list):
+    """
+    :param payloads_list: list containing output from the language model
+    :return: ranks and probabilities extracted by language model
+    """
+
+    ranks = []
+    probs = []
+    for payload in payloads_list:
+        for (rank, prob) in payload['real_topk']:
+            ranks.append(rank)
+            probs.append(prob)
+    return ranks, probs
 
 
 def get_list_of_files(dir_name):
@@ -20,3 +44,96 @@ def get_list_of_files(dir_name):
             all_files.append(full_path)
     return all_files
 
+
+def read_file(file_path):
+    """
+    :param file_path:  file path from which text is required
+    :return: text: text in the input file path
+    """
+
+    text = io.open(file_path, "r", errors="ignore").readlines()
+    text = ''.join(str(e) + "" for e in text)
+    text = text.strip()
+
+    return text
+
+
+def load_model(model_path):
+    """
+    :param model_path: path of model to load
+    :return: loaded model
+    """
+
+    with open(model_path + '.pkl', 'rb') as file:
+        pickle_model = pickle.load(file)
+    return pickle_model
+
+
+def extract_features(text, language_model, output_type, feature_type):
+    """
+    :param text: text for which the language model is required to extract outputs from
+    :param language_model: type of language model to be used
+    :param output_type: type of language model output to be returned
+    :param feature_type: type of feature to be returned
+    :return: values for features as a list
+    """
+
+    if 'bert' in language_model:
+        max_vocab_size = 28996
+    elif 'gpt2' in language_model:
+        max_vocab_size = 50257
+
+    logging.warning('Extracting output from language models !! Don\'t stop execution ...')
+    payloads = GLTR.main_code(text, language_model)
+    ranks, probs = get_ranks_and_probs(payloads)
+    logging.warning('Extracting features !! Don\'t stop execution ...')
+    if output_type == 'probs':
+        probs_create_plots(sorted(probs, reverse=True), 'dummy.png')
+    elif output_type == 'ranks':
+        ranks_create_plots(sorted(ranks, reverse=True), max_vocab_size, 'dummy.png')
+
+    if 'vgg' in feature_type:
+        features = get_VGG19_features('dummy.png')
+
+    os.remove("dummy.png")
+
+    return features
+
+
+def ranks_create_plots(ranks, maxsize, name):
+    """
+    :param ranks: list of ranks from text
+    :param maxsize: maximum rank
+    :param name: image name to be saved
+    """
+    x = list(range(1, len(ranks) + 1))
+    y = ranks
+    plt.axis('off')
+    plt.plot(x, y, color='#05D865')
+    plt.ylim(0, maxsize)
+    plt.savefig(name)
+    plt.close('all')
+
+
+def probs_create_plots(probs, name):
+    """
+    :param probs: list of probs from text
+    :param name: image name to be saved
+    """
+    x = list(range(1, len(probs) + 1))
+    y = probs
+    plt.axis('off')
+    plt.plot(x, y, color='#05D865')
+    plt.ylim(0, 1)
+    plt.savefig(name)
+    plt.close('all')
+
+def get_vgg19_features(img_path):
+    base_model = VGG19(weights='imagenet')
+    model = Model(inputs=base_model.input, outputs=base_model.get_layer('flatten').output)
+    img = image.load_img(img_path, target_size=(224, 224))
+    out = image.img_to_array(img)
+    out = np.expand_dims(out, axis=0)
+    out = preprocess_input(out)
+    flatten = model.predict(out)
+    return list(flatten[0])
